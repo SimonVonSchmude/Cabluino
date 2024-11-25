72e28b8e7fe695c96df9449e7bcd2d4882372b19
Simon von Schmude <simon-vonschmude@kh-berlin.de>
2024-11-25 18:03:31 +0100

    Adjust example

--- example/example.ino
+++ example/example.ino
@@ -6,7 +6,9 @@
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
-anchor<int> led11("/led11");
+anchor<int> led1("/led1");
+anchor<int> led2("/led2");
+anchor<int> led3("/led3");
 anchor<int> ledStripR("/ledstripR", LED_COUNT);
 anchor<int> ledStripG("/ledstripG", LED_COUNT);
 anchor<int> ledStripB("/ledstripB", LED_COUNT);
@@ -14,45 +16,33 @@ anchor<int> ledStripB("/ledstripB", LED_COUNT);
 long timer = 0;
 
 void setup() {
-  OSCSerial.begin(128000);
+  OSCSerial.begin(115200);
 
   pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();             // Turn OFF all pixels ASAP
-  strip.setBrightness(30);  // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(30);  // Set BRIGHTNESS to about 1/5 (max = 255)jjhjh
 }
 
 void loop() {
 
-OSCSerial.receive();
+  if (millis() - timer > 8) {
+    if(OSCSerial.receive()){
 
-analogWrite(11, led11.value[0]);
+    analogWrite(11, led1.value[0]);
+    analogWrite(10, led2.value[0]);
+    analogWrite(9, led3.value[0]);
 
-
-  // return;
-
-  // if (OSCSerial.receive()) {
-  //   digitalWrite(LED_BUILTIN, HIGH);
-
-  //   for (int i = 0; i < LED_COUNT; i++) {
-  //     strip.setPixelColor(i, strip.Color(ledStripR.value[i], ledStripG.value[i], ledStripB.value[i]));
-  //   }
-
-  //   strip.show();
-
-  // } else {
-  //   digitalWrite(LED_BUILTIN, LOW);
-  // }
-
-  if (millis() - timer > 16) {
     timer = millis();
 
     OSCBundle bndl;
-    bndl.add("/poti").add(analogRead(A0));
-    bndl.add("/value").add(led11.value[0]);
+    bndl.add("/value").add(led1.value[0]);
     bndl.add("/time/time").add((long)millis());
     OSCSerial.send(bndl);
-  }
+    }
+}
 }
